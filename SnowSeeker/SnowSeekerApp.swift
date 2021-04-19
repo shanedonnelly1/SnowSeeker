c00d9eb74e8eab5412e691eccd1c3fa7c8229d73
Shane Donnelly <shanedonnelly1@hotmail.com>
2021-04-19 09:01:18 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Shane on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
